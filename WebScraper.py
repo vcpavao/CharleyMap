11ebc4333aad38f8765a6cdce3a8d0b4b90c5235
Victor Pavao <vcpavao@gmail.com>
2018-07-09 18:56:15 -0400

    Add files via upload
    Prints out names of missing persons with last name beginning with 'A' alphabetically

--- WebScraper.py
+++ WebScraper.py
@@ -1,3 +1,24 @@
 from urllib.request import urlopen
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-print(html.read())
+from bs4 import BeautifulSoup
+import re
+
+html = urlopen("http://charleyproject.org/case-searches/alphabetical-cases")
+bs = BeautifulSoup(html, "html.parser")
+
+#Retrieves all <div> elements in the case class
+#nameList = bs.findAll("div",{"class":"case"})
+#href=re.compile("^(/case/)"
+#nameList = bs.findAll("a",)
+
+nameList = bs.findAll("div",{"class":"case"})
+                     
+for name in nameList :
+    print(name.get_text())
+print(len(nameList))
+                               
+#Need to go through every state, every name, take the data
+#Need to make their last seen location a map point
+#Need to add it to an OpenStreetMap
+#Need to make it a website
+
+#Filter through points with not enough information
