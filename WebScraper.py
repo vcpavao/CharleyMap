0a28d052bc7a5e5880c72c21d8c224056cacec3f
Victor Pavao <vcpavao@gmail.com>
2018-07-09 20:38:19 -0400

    Edited scraper file
    Prints out URLs of all 'A' cases

--- WebScraper.py
+++ WebScraper.py
@@ -5,20 +5,16 @@ import re
 html = urlopen("http://charleyproject.org/case-searches/alphabetical-cases")
 bs = BeautifulSoup(html, "html.parser")
 
-#Retrieves all <div> elements in the case class
-#nameList = bs.findAll("div",{"class":"case"})
-#href=re.compile("^(/case/)"
-#nameList = bs.findAll("a",)
-
-nameList = bs.findAll("div",{"class":"case"})
-                     
-for name in nameList :
-    print(name.get_text())
-print(len(nameList))
+#Retrieves all case links and removes formatting
+for link in bs.find("div",{"class":"columns"}).findAll("a") :
+    #Prints link
+    print(link.attrs['href'])
+    html = link
+    for case in bs.find("div", {"class":"column"}).findAll("li") :
+        print(case)
+    #Retrieve h1 and all (column) li from 
                                
-#Need to go through every state, every name, take the data
 #Need to make their last seen location a map point
 #Need to add it to an OpenStreetMap
 #Need to make it a website
-
-#Filter through points with not enough information
+#Filter through points with not enough informati
